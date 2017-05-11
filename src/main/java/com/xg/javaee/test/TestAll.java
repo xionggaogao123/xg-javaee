fc6f777da7a70a12d8dd89dc8d797313838a78d5
颜鑫宇 <yanxinyu@terminus.io>
2017-05-11 14:53:06 +0800

    测试

---
+++ src/main/java/com/xg/javaee/test/TestAll.java
@@ -0,0 +1,16 @@
+package com.xg.javaee.test;
+
+/**
+ * Created by xionggao on 2017/5/11.
+ */
+public class TestAll {
+
+    public static void main(String[] args){
+        String hello = getHelloWorld();
+        System.out.println("****  hello *****"+hello);
+    }
+
+    public static String getHelloWorld (){
+        return "hello world";
+    }
+}
